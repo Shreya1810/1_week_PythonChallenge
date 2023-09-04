7693cf421fff43fae56a3d99ba621994d8e739d6
Shreya1810 <66972361+Shreya1810@users.noreply.github.com>
2023-09-04 18:44:07 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,8 @@
+'''Day 1: Variables and Input*
+Create a Python program that asks the user for their name and age, then prints a message that includes their name and age. Use variables to store the user's input and display the message.
+'''
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+
+
+print(f'Your name is {name} and you are {age} years old')
